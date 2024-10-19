490af1b89e5d33aaf205e0717fd0e0d791ee5312
Alireza Babaei <alireza.bs936@gmail.com>
2024-10-19 21:49:57 +0330

    third version

---
+++ about-us-page/about-us.js
@@ -0,0 +1,59 @@
+        const menu_first = document.getElementById("first-social-menue");
+        const menu_second = document.getElementById("second-social-menue");
+        const menu_third = document.getElementById("third-social-menue");
+        const menu_forth = document.getElementById("forth-social-menue");
+        const icon_fisrt = document.getElementById("first-icon-change");
+        const icon_second = document.getElementById("second-icon-change");
+        const icon_third = document.getElementById("third-icon-change");
+        const icon_forth = document.getElementById("forth-icon-change");
+        const icon_fifth = document.getElementById("fifth-icon-change");
+        const icon_sixth = document.getElementById("sixth-icon-change");
+        const icon_seventh = document.getElementById("seventh-icon-change");
+        const icon_eight = document.getElementById("eight-icon-change");
+        function toggleMenu_first() {
+        menu_first.classList.toggle("open");
+        icon_fisrt.classList.toggle("d-none");
+        icon_second.classList.toggle("d-none");
+      }
+      function toggleMenu_second(){
+        menu_second.classList.toggle("open");
+        icon_third.classList.toggle("d-none");
+        icon_forth.classList.toggle("d-none");
+      }
+      function toggleMenu_third(){
+        menu_third.classList.toggle("open");
+        icon_fifth.classList.toggle("d-none");
+        icon_sixth.classList.toggle("d-none");
+      }
+      function toggleMenu_forth(){
+        menu_forth.classList.toggle("open");
+        icon_seventh.classList.toggle("d-none");
+        icon_eight.classList.toggle("d-none");
+      }
+
+    //   const menu_first = document.getElementById("first-social-menue");
+    //   const menu_second = document.getElementById("second-social-menue");
+    //   const menu_third = document.getElementById("third-social-menue");
+    //   const menu_forth = document.getElementById("forth-social-menue");
+    //   const icon_first = document.getElementById("first-icon-change");
+    //   const icon_second = document.getElementById("second-icon-change");
+    //   const icon_third = document.getElementById("third-icon-change");
+    //   const icon_forth = document.getElementById("forth-icon-change");
+    //   function toggleMenu_first() {
+    //   menu_first.classList.toggle("open");
+    //   icon_first.classList.toggle("d-none");
+    // }
+    // function toggleMenu_second() {
+    //   menu_second.classList.toggle("open");
+    //   icon_second.classList.toggle("d-none");
+
+    // }
+    // function toggleMenu_third() {    
+    //   menu_third.classList.toggle("open");
+    //   icon_third.classList.toggle("d-none");
+
+    // }
+    // function toggleMenu_forth() {  
+    //   menu_forth.classList.toggle("open");
+    //   icon_forth.classList.toggle("d-none");
+    // }
